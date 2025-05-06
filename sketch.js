a99060130367aca5a641c1f0640049d7d0a33cf7
AngelicaGabriellaTrinchillo <angelica.trinchillo.86@gmail.com>
2025-05-06 17:10:18 +0200

    più image

--- sketch.js
+++ sketch.js
@@ -1,15 +1,83 @@
+/**
+ * @typedef {import("./p5/types").Graphics} Graphics
+ * @typedef {import("./p5/types").Image} Image
+ *
+ * @typedef {Object} Cubo
+ * @property {number} x
+ * @property {number} y
+ * @property {number} z
+ * @property {number} size
+ * @property {string} color
+ * @property {function} rotationFunction
+ */
+
+//
+
+/** @type {Cubo[]} */
+let cubi = [];
+
+let copie = 50;
+
+/** @type {Graphics} */
+let g;
+
+/** @type {Image} */
+let img;
+
+function preload() {
+  img = loadImage("./image/logo-08.jpg");
+}
+
+//
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, "webgl");
+
+  g = createGraphics(100, 100);
+
+  let distanza = 500;
+  for (let i = 0; i < copie; i++) {
+    let cubo = {
+      x: random(-distanza, distanza),
+      y: random(-distanza, distanza),
+      z: random(-distanza, distanza),
+      size: 100,
+      color: random(["pink", "yellow", "blue"]),
+      rotationFunction: random([rotateX, rotateY]),
+    };
+    cubi.push(cubo);
+  }
 }
 
 function draw() {
-  background(220);
-  const s = 100;
+  background("white");
+  orbitControl();
+  rotateY(frameCount * 0.001);
+  noStroke();
 
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+  g.background("white");
+  g.push();
+  g.translate(g.width / 2, g.height / 2);
+  g.imageMode("center");
+  g.scale(g.width / img.width);
+  // g.text("trame urbane", 0, g.height);
+  // g.textSize(g.height);
+  g.image(img, 0, 0);
+  g.pop();
+
+  texture(g);
+
+  for (let cubo of cubi) {
+    push();
+    translate(cubo.x, cubo.y, cubo.z);
+
+    let velocita = frameCount * 0.005;
+    cubo.rotationFunction(velocita);
+    rotateZ(velocita);
+
+    box(cubo.size);
+    pop();
+  }
 }
 
 function windowResized() {
